a5e9edeb1f23fe7f7144ca225e775163e99d24b3
AndreasErgenzinger <andreas.ergenzinger@gmx.de>
2013-04-10 15:29:54 +0200

    deleted unneccessary files

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-#require_once 'lib/login_lib.php';
-require_once '../../lib/base.php';
-#\OCA\user_shibboleth\Auth::isAuthenticated();
-
-\OCA\user_shibboleth\LoginLib::printAutoLoginPage('test');
